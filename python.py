4a20df165043f27526f88bca19904e9699cb6cdd
taj1224 <150121899+taj1224@users.noreply.github.com>
2024-11-12 13:43:48 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,9 @@
+# Get user input for two numbers
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Calculate the sum
+sum = num1 + num2
+
+# Display the result
+print("The sum of {0} and {1} is {2}".format(num1, num2, sum))
